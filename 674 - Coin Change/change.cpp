22d13832c37ed0fcf38b952ef64d232a1c1600ba
Yuting Zhang <ytyt96@live.com>
2017-02-26 16:27:27 -0600

    optimized

--- 674 - Coin Change/change.cpp
+++ 674 - Coin Change/change.cpp
@@ -3,20 +3,18 @@
 
 using namespace std;
 
-// run time was 2.982s and time limit is 3.000s.
-// Probably should just use built-in arrays rather than vectors
 int main(){
     int coins[5] = {1, 5, 10, 25, 50};
     int money;
-    while (cin >> money)
-    {
-        vector<int> dp(money + 1, 0);
-        dp[0] = 1;
-        for (int i = 0; i < 5; ++i)
-            for (int j = 1; j <= money; ++j)
-                if (j - coins[i] >= 0)
-                    dp[j] += dp[j - coins[i]];
-        cout << dp[money] << "\n";
+    int dp[7500] = {0};
+    dp[0] = 1;
+    for (int i = 0; i < 5; ++i)
+        for (int j = 1; j <= 7489; ++j)
+            if (j - coins[i] >= 0)
+                dp[j] += dp[j - coins[i]];
+
+    while (scanf("%d", &money) == 1) {
+        printf("%d\n", dp[money]);
     }
     return 0;
 }
